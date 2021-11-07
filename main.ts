b929899563b9b36f92f05b1d2465a6898fdd851c
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-11-07 17:31:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let fire = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    fire = pins.digitalReadPin(DigitalPin.P1)
+    if (fire == 0) {
+        basic.showIcon(IconNames.Angry)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+    basic.showNumber(fire)
 })
